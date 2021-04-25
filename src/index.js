ce21c9719f84e81b7390af7c92d712ffe57a3c96
Jeremy <jeremyVDLRC@gmail.com>
2021-04-25 16:05:29 +1000

    Change file name and test square component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './Main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   //$FlowIgnore
   document.getElementById('root')
