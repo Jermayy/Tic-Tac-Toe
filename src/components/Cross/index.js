a42ac5ba8c0dc8815e020220e056d9f2c82cb93b
Jeremy <jeremyVDLRC@gmail.com>
2021-04-25 18:30:28 +1000

    create cross component

---
+++ src/components/Cross/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const Cross = (): React.Node => {
+    const Styles = {
+        container: {}
+    }
+    return(
+        <div style={Styles.container}>
+        X
+        
+        </div>
+    )
+}
+
+export default Cross;
