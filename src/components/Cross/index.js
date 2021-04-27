07d95179c1850d32fe131b2262382ece2f983f8b
Jeremy <jeremyVDLRC@gmail.com>
2021-04-27 21:32:32 +1000

    Create and render Cross Component

--- src/components/Cross/index.js
+++ src/components/Cross/index.js
@@ -1,14 +1,36 @@
+// @flow
 import * as React from 'react';
 
 const Cross = (): React.Node => {
     const Styles = {
-        container: {}
+        container:{
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+        },
+        side1: {
+            backgroundColor: 'red',
+            width: '10%',
+            height: '90%',
+            left: '5%',
+            position: 'relative',
+            transform: 'rotate(45deg)',
+        },
+        side2: {
+            backgroundColor: 'red',
+            height: '90%',
+            position: 'relative',
+             right: '5%',
+            transform: 'rotate(135deg)',
+            width: '10%',
+        }
     }
+    
     return(
-        <div style={Styles.container}>
-        X
-        
-        </div>
+       <>
+         <div style={Styles.side1}> </div>
+         <div style={Styles.side2}> </div>        
+       </>
     )
 }
 
