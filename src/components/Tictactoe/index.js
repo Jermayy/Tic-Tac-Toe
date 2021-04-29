6cf8d5689852db7308b5b1ae56677442b8af1028
Jeremy <jeremyVDLRC@gmail.com>
2021-04-29 14:39:11 +1000

    Add squares and state to tictactoe component

--- src/components/Tictactoe/index.js
+++ src/components/Tictactoe/index.js
@@ -4,35 +4,52 @@ import * as React from 'react';
 import Square from '../Square';
 
 const Tictactoe = ():React.Node => {
+
         const Empty = 'Empty';
         const Circle = 'Circle';
         const Cross = 'Cross';
 
-        const positions = [
-            Circle, Circle ,Cross,
-            Empty, Empty, Circle,
-            Cross, Cross, Empty
-        ]
+        
+    const [state, setState] = React.useState({
+   player: Circle,
+   positions:[
+            Empty, Empty, Empty,
+            Empty, Empty, Empty,
+            Empty, Empty, Empty
+        ],
+    });
+     
+    const takeTurn = (position) => {
+        const positions = [...state.positions];
+        positions[position] = state.player;
+
+        setState({
+            player: state.player === Circle ? Cross : Circle,
+            positions,
+        })
+    }
+
+
     const styles = {
         grid: {
-display: 'grid',
-gridTemplateColumns: 'auto auto auto',
-gridTemplateRows: 'auto auto auto',
-gap: '10px',
-backgroundColor: 'lightgrey',
+            display: 'grid',
+            gridTemplateColumns: 'auto auto auto',
+            gridTemplateRows: 'auto auto auto',
+            gap: '10px',
+            backgroundColor: 'lightgrey',
         }
     }
     return(
         <div style={styles.grid}>
-            <Square position={0} value ={positions[0]}/>
-            <Square position={1} value ={positions[1]}/>
-            <Square position={2} value ={positions[2]}/>
-            <Square position={3} value ={positions[3]}/>
-            <Square position={4} value ={positions[4]}/>
-            <Square position={5} value ={positions[5]}/>
-            <Square position={6} value ={positions[6]}/>
-            <Square position={7} value ={positions[7]}/>
-            <Square position={8} value ={positions[8]}/>
+            <Square position={0} value ={state.positions[0]} takeTurn={takeTurn}/>
+            <Square position={1} value ={state.positions[1]} takeTurn={takeTurn}/>
+            <Square position={2} value ={state.positions[2]} takeTurn={takeTurn}/>
+            <Square position={3} value ={state.positions[3]} takeTurn={takeTurn}/>
+            <Square position={4} value ={state.positions[4]} takeTurn={takeTurn}/>
+            <Square position={5} value ={state.positions[5]} takeTurn={takeTurn}/>
+            <Square position={6} value ={state.positions[6]} takeTurn={takeTurn}/>
+            <Square position={7} value ={state.positions[7]} takeTurn={takeTurn}/>
+            <Square position={8} value ={state.positions[8]} takeTurn={takeTurn}/>
         </div>
     )
 }
