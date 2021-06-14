2d0d713e9a6da88323b8d09faa659c0b964c63af
Jeremy <jeremyVDLRC@gmail.com>
2021-06-14 12:19:47 +1000

    added styling to result box

--- src/components/Tictactoe/Result.js
+++ src/components/Tictactoe/Result.js
@@ -10,18 +10,30 @@ const Result = ({ winner }: Props):React.Node => {
   const Styles = {
     container: {
       border: 'solid',
+      borderRadius: '20px',
+      textAlign: 'center',
+      padding: '30px',
+      margin: '20px',
+    },
+    text: {
+      padding: '20px',
+      fontWeight: '700',
+    },
+    button: {
+      padding: '20px',
+      fontWeight: '700',
     },
 
   };
 
   return (
     <div style={Styles.container} data-testid="result">
-      <div>
+      <div style={Styles.text}>
         {winner === 'Circle' && 'Circle Wins!'}
         {winner === 'Cross' && 'Cross Wins!'}
         {winner === 'Tie' && "It's a Tie!"}
       </div>
-      <button type="button" onClick={() => { window.location.reload(); }}> Reset Game</button>
+      <button type="button" onClick={() => { window.location.reload(); }} style={Styles.button}> Reset Game</button>
     </div>
 
   );
